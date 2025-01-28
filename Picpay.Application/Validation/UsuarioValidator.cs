7595af3156d6d8a227e62cdc0b670d7070566a1c
Otavio Augusto Reis Almeida <otavio.almeida@pwi.com.br>
2025-01-28 11:28:24 -0300

    feat: criando todas as classes da camada Application

---
+++ Picpay.Application/Validation/UsuarioValidator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Picpay.Application.Validation
+{
+    public class UsuarioValidator
+    {
+    }
+}
